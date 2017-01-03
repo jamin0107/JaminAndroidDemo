1ce0b4aa0793eda9702f33c8d266c90cdfcbaa7d
Jamin <jamin0107@gmail.com>
2017-01-03 12:13:40 +0800

    update iconfont.

--- app/src/main/java/com/jamin/android/demo/JaminApplication.java
+++ app/src/main/java/com/jamin/android/demo/JaminApplication.java
@@ -1,8 +1,14 @@
 package com.jamin.android.demo;
 
 import android.app.Application;
+import android.text.TextUtils;
 
 import com.jamin.framework.util.LogUtil;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by jamin on 2016/11/25.
@@ -10,12 +16,41 @@ import com.jamin.framework.util.LogUtil;
 
 public class JaminApplication extends Application {
 
+    String mProcName;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        LogUtil.init(true , "Jamin");
+        LogUtil.init(true, "Jamin");
+        mProcName = getProcessName();
+        LogUtil.d("PROCESS : ----" + mProcName);
     }
 
 
+    public String getProcessName() {
+//        File cmdFile = new File("/proc/self/cmdline");
+//
+//        if (cmdFile.exists() && !cmdFile.isDirectory()) {
+//            BufferedReader reader = null;
+//            try {
+//                reader = new BufferedReader(new InputStreamReader(new FileInputStream(cmdFile)));
+//                String procName = reader.readLine();
+//                LogUtil.d("/proc/self/cmdline procName:" + procName);
+//                if (!TextUtils.isEmpty(procName)) {
+//                    return procName.trim();
+//                }
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            } finally {
+//                if (reader != null) {
+//                    try {
+//                        reader.close();
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//        }
+        return getApplicationInfo().processName;
+    }
 }
