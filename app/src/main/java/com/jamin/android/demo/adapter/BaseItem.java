6e4517d59e6527378797778b542e6c6913378e91
Jamin.Wang <jamin0107@gmail.com>
2017-09-29 09:37:30 +0800

    update.

--- app/src/main/java/com/jamin/android/demo/adapter/BaseItem.java
+++ app/src/main/java/com/jamin/android/demo/adapter/BaseItem.java
@@ -1,5 +1,6 @@
 package com.jamin.android.demo.adapter;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 
 import com.jamin.android.demo.ui.base.BaseActivity;
@@ -11,6 +12,7 @@ public abstract class BaseItem<Data> {
 
     private Data mData;
     private BaseActivity activity;
+    private Fragment fragment;
 
 
     public BaseItem(BaseActivity activity) {
@@ -23,18 +25,42 @@ public abstract class BaseItem<Data> {
         this.mData = data;
     }
 
+    public BaseItem(Fragment fragment) {
+        this.fragment = fragment;
+    }
+
+
+    public BaseItem(Fragment fragment, Data data) {
+        this.fragment = fragment;
+        this.mData = data;
+    }
+
+
     public abstract int getLayoutId();
+
 
     public abstract void onBindView(BaseHolder holder, int position);
 
 
-    public final FragmentActivity getActivity() {
-        return activity;
+    protected final FragmentActivity getActivity() {
+        if (activity != null) {
+            return activity;
+        }
+        if (fragment != null) {
+            return fragment.getActivity();
+        }
+        return null;
+    }
+
+
+    protected final Fragment getFragment() {
+        return fragment;
     }
 
 
     /**
      * 返回Item绑定的数据
+     *
      * @return
      */
     public Data getItemData() {
